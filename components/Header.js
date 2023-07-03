eeadf3b581d3b41c55ef38d2aae13d5f02a55444
Aayush <ayushkarkee6@gmail.com>
2023-07-03 19:47:35 +0545

    added logic to add, update and delete list

--- components/Header.js
+++ components/Header.js
@@ -5,14 +5,12 @@ const Header = () => {
     return (
         <header class="text-gray-600 body-font">
             <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-                <a class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+                <div class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
                     <span class="ml-3 text-xl">ToDo List</span>
-                </a>
+                </div>
                 <nav class="md:ml-auto flex flex-wrap items-center text-base justify-center">
-                    <Link href={"/"} class="mr-5 hover:text-gray-900 cursor-pointer">Home</Link>
-                    <Link href={"/"}class="mr-5 hover:text-gray-900 cursor-pointer">about</Link>
-                    <Link href={"/todos"}class="mr-5 hover:text-gray-900 cursor-pointer">My-ToDos</Link>
-                    <Link href={"/"}class="mr-5 hover:text-gray-900 cursor-pointer">Contact US</Link>
+                    <Link href={"/"} class="mr-10 hover:text-gray-900 cursor-pointer">Home</Link>
+                    <Link href={"/todos"}class="mr-10 hover:text-gray-900 cursor-pointer">My-ToDos</Link>
                 </nav>
             </div>
         </header>
